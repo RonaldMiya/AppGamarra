ca6962b306f7a68cc14aa0abf0980098967a7c2f
RonaldMiya <ronald.miya@tecsup.edu.pe>
2018-03-05 11:42:59 -0500

    Commit interfaces 3 y 4

---
+++ src/main/java/com/busnet/app/GamarraApp/Intefaces/Ropas_Damas.java
@@ -0,0 +1,37 @@
+package com.busnet.app.GamarraApp.Intefaces;
+
+public class Ropas_Damas {
+	
+	
+	public String InformePrenda() 
+	{
+		 String prenda;
+		 
+		 prenda = "Falda";
+		
+		return prenda; 
+	}
+	public String InformeDescripsion() 
+	{
+		 String descripción;
+		 
+		 descripción = "Tela polistel";
+		
+		return descripción; 
+	}
+	public String InformeTalla() 
+	{
+		 String talla;
+		 talla = "M";
+		
+		return talla;
+	}
+	public Double InformePrecio() 
+	{
+		 Double precio;
+		 precio = 20.0;
+		
+		return precio;
+	}
+
+}
---
+++ src/main/java/com/busnet/app/GamarraApp/Intefaces/Ropas_ninios.java
@@ -0,0 +1,31 @@
+package com.busnet.app.GamarraApp.Intefaces;
+
+public class Ropas_ninios {
+	
+	
+	public String InformePrenda() 
+	{
+		 String prenda;
+		 
+		 prenda = "Short";
+		
+		return prenda; 
+	}
+	
+	public String InformeImagen() 
+	{
+		 String imagen;
+		 imagen = "short.jpg";
+		
+		return imagen;
+	}
+
+	public Double InformePrecio() 
+	{
+		 Double precio;
+		 precio = 5.0;
+		
+		return precio;
+	}
+
+}
